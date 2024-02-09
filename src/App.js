5700cd46c3972e480e461923a160c353fc05a046
bradleyham <bradleyjamesham@gmail.com>
2024-02-09 14:52:52 +1300

    responsive about section added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home/Hero/Hero';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
-         <Home />
       </header>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/AboutSection/AboutSection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './aboutSection.scss';
+
+function AboutSection() {
+  return (
+    <section className='aboutSection'>
+        <div className='aboutSection-content'>
+            <h2 className='h2-small'>About Us</h2>
+            <h2 className='h2-large'>Your premier partner for advanced
+masonry protection</h2>
+            <div className='about-image'></div>
+            <p>Welcome to RTS Construction Chemicals, your premier source for advanced masonry solutions, anti-graffiti coatings, and liquid waterproofing systems. Our commitment is to safeguard your structures with cutting-edge technology and eco-friendly solutions. Shield your masonry investments with our comprehensive range of services tailored to protect and enhance. We specialise in professional sealing solutions which provide an impermeable barrier, safeguarding against moisture and staining preserving the integrity of your surfaces. Anti-graffiti coatings for surface preservation, and liquid waterproofing systems for comprehensive protection against water damage. Our team of experts ensures tailored solutions for every project, prioritising quality, innovation, and customer
+satisfaction. </p>
+
+
+        </div>
+    </section>  
+  )
+}
+
+export default AboutSection
